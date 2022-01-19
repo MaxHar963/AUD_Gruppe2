96affa7869d73190b761c5dee5afea9edf9aa784
gschmidbauer18 <Schmidbauer.Gabriel@sus.htl-grieskirchen.at>
2022-01-19 15:36:39 +0100

    Test

--- AUD_Gruppe2/AUD_Gruppe2/Program.cs
+++ AUD_Gruppe2/AUD_Gruppe2/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿List<string> listImages = new List<string>();
+string[] files;
+
+Console.WriteLine("Path to Picture-Folder:");
+string path = Console.ReadLine();
+//Console.WriteLine(path);
+Console.WriteLine(" ");
+
+if (path != null && path != "")
+{
+    files = Directory.GetFiles(path);
+
+    foreach (var file in files)
+    {
+        listImages.Add(file);
+        //Console.WriteLine("Path: " + path);
+        //Console.WriteLine("Filename: " + file);
+        //Console.WriteLine(" ");
+    }
+
+    foreach (var file in listImages)
+    {
+        Console.WriteLine(file);
+        Console.WriteLine(" ");
+    }
+}
